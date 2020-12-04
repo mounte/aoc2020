9f4bed5dff27dbde44ae54bda6f90a9d6a0141f5
Daniel Wedlund <daniel@wedlund.eu>
2020-12-04 02:06:55 +0100

    day3

--- run_1.py
+++ run_1.py
@@ -1,9 +1,25 @@
 f = "sample.txt"
 f = "input.txt"
 
+def check_trees(r):
+    return [1 if ch == "#" else 0 for ch in r]
 
 def main():
-    d = open(f, "rt").readlines()
+    lines = open(f, "rt").readlines()
+    rows = list(map(check_trees, lines))
+    col = 3
+    stride = len(rows[0])-1
+    step_size = 3
+    # step 3 right, one down
+    ct = 0
+    for r in rows[1:]:
+        ct += r[col]
+        col += step_size
+        col %= stride
+
+    print(ct)
+
+
 
 if __name__ == "__main__":
     main()
--- run_2.py
+++ run_2.py
@@ -1,9 +1,30 @@
+import math
+
 f = "sample.txt"
 f = "input.txt"
 
+def check_trees(r):
+    return [1 if ch == "#" else 0 for ch in r]
 
 def main():
-    d = open(f, "rt").readlines()
+    lines = open(f, "rt").readlines()
+    rows = list(map(check_trees, lines))
+    
+    stride = len(rows[0])-1
+    vals = []
+    for step_size, row_jumps in ((1,1), (3,1), (5,1), (7,1), (1,2)):
+        # step 3 right, one down
+        print(f"testing step_size={step_size}, row_jump={row_jumps}")
+        ct = 0
+        col = 0
+        for r in rows[::row_jumps]:
+            ct += r[col]
+            col += step_size
+            col %= stride
+        vals.append(ct)
+        print(ct)
+    print(math.prod(vals))
+
 
 if __name__ == "__main__":
     main()
