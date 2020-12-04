fb78f0290d2b204dbedb210f2849b1cefa86890d
Daniel Wedlund <daniel@wedlund.eu>
2020-12-04 01:44:49 +0100

    day2

--- run_1.py
+++ run_1.py
@@ -1,9 +1,18 @@
 f = "sample.txt"
 f = "input.txt"
 
-
 def main():
-    d = open(f, "rt").readlines()
+    d = map(str.split, open(f, "rt").readlines())
+    valid = 0
+    for a, b, c in d:
+        count_low, count_high = map(int, a.split('-'))
+        ch = b[0]
+        ct = c.count(ch)
+        if count_low <= ct <= count_high:
+            valid += 1
+
+    print(f"{valid}")
+
 
 if __name__ == "__main__":
     main()
--- run_2.py
+++ run_2.py
@@ -1,9 +1,23 @@
 f = "sample.txt"
 f = "input.txt"
 
-
 def main():
-    d = open(f, "rt").readlines()
+    d = map(str.split, open(f, "rt").readlines())
+    valid = 0
+    for a, b, c in d:
+        pos_low, pos_high = map(int, a.split('-'))
+        ch = b[0]
+        ct = 0
+        if c[pos_low-1] == ch:
+            ct += 1
+        if c[pos_high-1] == ch:
+            ct += 1
+
+        if ct == 1:
+            valid += 1
+
+    print(f"{valid}")
+
 
 if __name__ == "__main__":
     main()
