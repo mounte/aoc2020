56bd0cd8ec142540d413bb735f36fcbf316811aa
Daniel Wedlund <daniel@wedlund.eu>
2020-12-04 01:35:13 +0100

    base

--- run_1.py
+++ run_1.py
@@ -1,10 +1,9 @@
+f = "sample.txt"
 f = "input.txt"
-target = 2020
 
-d = set(map(int, open(f, "rt").readlines()))
-for v in d:
-    if target-v in d:
-        print(f"{v*(target-v)}")
-        break
-else:
-    print("No solution")
+
+def main():
+    d = open(f, "rt").readlines()
+
+if __name__ == "__main__":
+    main()
--- run_2.py
+++ run_2.py
@@ -1,19 +1,9 @@
 f = "sample.txt"
 f = "input.txt"
-target = 2020
 
-d = set(map(int, open(f, "rt").readlines()))
-dd = sorted(d)
-found = False
-for idx, v0 in enumerate(dd):
-    for v1 in dd[idx+1:]:
-        if v0+v1 > target:
-            break
-        if target-v0-v1 in d:
-            print(f"{v0*v1*(target-v0-v1)}")
-            found = True
-            break
-    if found:
-        break
-else:
-    print("No solution")
+
+def main():
+    d = open(f, "rt").readlines()
+
+if __name__ == "__main__":
+    main()
