add9ca65b4bff16943a711e451686b9c82b98720
Daniel Wedlund <daniel@wedlund.eu>
2020-12-15 02:04:01 +0100

    day8

--- run_1.py
+++ run_1.py
@@ -3,7 +3,29 @@ f = "input.txt"
 
 
 def main():
-    d = open(f, "rt").readlines()
+    instructions = []
+    for line in open(f, "rt"):
+        instr, para = line.rstrip().split()
+        para = int(para)
+        instructions.append((instr, para))
+
+    ip = 0
+    reg = 0
+    processed_instructions = set()
+
+    while 0 <= ip < len(instructions):
+        op, param = instructions[ip]
+        if ip in processed_instructions:
+            break
+        processed_instructions.add(ip)
+
+        if op == "jmp":
+            ip += (param - 1)
+        elif op == "acc":
+            reg += param
+        ip += 1
+    print(reg)
+
 
 if __name__ == "__main__":
     main()
--- run_2.py
+++ run_2.py
@@ -3,7 +3,47 @@ f = "input.txt"
 
 
 def main():
-    d = open(f, "rt").readlines()
+    instructions = []
+    fudge_index = [-1]
+    for idx, line in enumerate(open(f, "rt")):
+        instr, para = line.rstrip().split()
+        para = int(para)
+        if instr in ("nop", "jmp"):
+            fudge_index.append(idx)
+
+        instructions.append((instr, para))
+
+    def run_prog(program):
+        ip = 0
+        reg = 0
+        processed_instructions = set()
+        while 0 <= ip < len(program):
+            op, param = program[ip]
+            if ip in processed_instructions:
+                return False
+
+            processed_instructions.add(ip)
+            if op == "jmp":
+                ip += (param-1)
+            elif op == "acc":
+                reg += param
+            ip += 1
+
+        print(reg)
+        return True
+
+    for fi in fudge_index:
+        the_prog = instructions[:]
+        if fi >= 0:
+            old_instr, param = the_prog[fi]
+            if old_instr == "jmp":
+                new_instr = "nop", param
+            else:
+                new_instr = "jmp", param
+            the_prog[fi] = new_instr
+
+        if run_prog(the_prog):
+            break
 
 if __name__ == "__main__":
     main()
